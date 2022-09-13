072635ff40df48770dc2eb916692ee2503064bb3
DESKTOP-I7LQ7BU\a <cloudvision@naver.com>
2022-09-13 09:33:43 +0900

    python

---
+++ Joy.Joo/Python-App/python start.py
@@ -0,0 +1,18 @@
+def add(x,y):
+    return x + y
+
+def  sub(x,y):
+    return x - y 
+
+
+total = add(10,20)
+print(total)
+
+f = add  #add 함수에 대한 참조.
+total = f(20,30)
+print(total)
+
+def test(f):   # f=add
+    return f(30,40) #함수를 호출하겠다.
+
+test(add)
